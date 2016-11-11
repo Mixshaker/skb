e822934cc4f25686aee90d9f42cfd6e7d71638aa
Rostislav <stillgrey@mail.ru>
2016-11-11 03:39:50 +0200

    task2b

--- src/index.js
+++ src/index.js
@@ -12,11 +12,43 @@ app.get('/', (req, res) => {
 
 app.get('/task2a', (req, res) => {
   const sum = (+req.query.a || 0) + (+req.query.b || 0);
-
   res.send(sum.toString());
 });
 
 
+app.get('/task2b', (req, res) => {
+  var newName = "";
+  var arr;
+
+  var fullName = req.query.fullname;
+  if (fullName.match(/^[A-Za-zА-Яа-я ]+/i)) {
+
+  arr = fullName.split(" ");
+
+    if (fullName == "" || arr.length > 3) {
+      newName = 'Invalid fullname';
+    }
+    else if (arr.length == 3){
+    const fName = arr[0].substring(1, 0) + ".";
+    const lName = arr[2];
+    const sName = arr[1].substring(1, 0) + ".";
+    newName = `${lName} ${fName} ${sName}`;
+    }
+    else if (arr.length == 2){
+    const fName = arr[0].substring(1, 0) + ".";
+    const lName = arr[1];
+    newName = `${lName} ${fName}`;
+    }
+    else{
+    const lName = arr[0];
+    newName = `${lName}`;
+    }
+  }else{
+    newName = 'Invalid fullname';
+  }
+
+  res.send(newName);
+});
 
 
 app.listen(3000, () => {
